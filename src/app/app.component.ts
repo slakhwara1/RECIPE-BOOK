3ed5d867c37921a00ddfb45946fe37b56d030c9a
Shivam Lakhwara <shivam.lakhwara@mavenwave.com>
2020-02-14 17:54:21 +0530

    Replaced EventEmitter with Subject and some minor observable refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  title = 'recipe-book';
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
-
 }
